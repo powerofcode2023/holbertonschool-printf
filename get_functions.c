0f1311349e7b13f6a9596c2c76be416f221017d9
abdel <holbertonc21@gmail.com>
2023-07-26 15:05:19 -0700

    create get_functions.c file

---
+++ get_functions.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int (*get_functions(char *format))(va_list)
+{
+    int i = 0;
+
+
+    spec array[]=
+    {
+        {"c", printt_char},
+        {"s", print_string},
+        {"%", print_percentage},
+	{"d", print_d},
+	{"i", print_d},
+        {"NULL", NULL},
+    };
+    while(array[i].p)
+    {
+        if (*format  == array[i].p[0])
+        return (array)[i].f);
+        i++;
+    }
+    return (NULL);
+}
