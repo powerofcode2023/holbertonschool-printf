295e3374e9b73e05246b28dffbedbd3022384010
abdel <holbertonc21@gmail.com>
2023-07-26 10:05:24 -0700

    create the main.h file, and add some prototypes

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format,...);
+int _putchar(char c);
+
+#endif
