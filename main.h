b70a52864cf94dd72706ac4e81eee22fa287144a
abdel <holbertonc21@gmail.com>
2023-07-26 15:07:09 -0700

    add typedef struct & fix forgotten ";"

--- main.h
+++ main.h
@@ -6,11 +6,23 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct specifier - struct specifier
+ * @p: the right character
+ * @f: the right function
+ */
+typedef struct specifier
+{
+	char *p;
+	int (*f)(va_list args);
+}spec;
+
 int _printf(const char *format,...);
 int _putchar(char c);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percentage(va_list args);
 int print_d(va_list args);
+int (*get_functions(char *format))(va_list);
 
 #endif
