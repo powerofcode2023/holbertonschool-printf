75f8f7014f0e218fcf0da2f370975e3f2fec697b
abdel <holbertonc21@gmail.com>
2023-07-26 14:18:31 -0700

    write print_d function

--- functions.c
+++ functions.c
@@ -43,6 +43,7 @@ int print_string(va_list args)
 /**
  * print_percentage - print a character '%'
  * @args: percentage as argument
+ *
  * Return: 1 is the length of the character, here '%'
  */
 
@@ -52,4 +53,38 @@ int print_percentage(va_list args)
 	_putchar('%');
 
 	return (1);
+}
+
+/** print_d - print a decimal
+ * args: decimal as argument
+ *
+ * Return: length of the number
+ */
+int print_d(va_list args)
+{
+	int n = va_arg(args, int);
+	unsigned int absolute;
+	int count = 0;
+
+	if (n < 0)
+	{
+		absolute = (unsigned int)(-n);
+		count += _putchar('-');
+	}else
+	{
+		absolute = (unsigned int)n;
+	}
+	unsigned int countnum = 1;
+	unsigned int aux = absolute;
+	while (aux > 9)
+	{
+		aux /= 10;
+		countnum *= 10;
+	}
+	while (countnum >= 1)
+	{
+		count += _putchar((absolute / countnum) %10 + '0');
+		countnum /= 10;
+	}
+	return count;
 }
