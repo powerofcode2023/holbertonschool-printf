7221ce57acce023dce5815690f0ae8dcc1c77fd7
abdel <holbertonc21@gmail.com>
2023-07-27 01:43:03 -0700

    betty style fix

--- get_functions.c
+++ get_functions.c
@@ -1,23 +1,27 @@
 #include "main.h"
 
+/**
+ * get_functions - look for the specifier
+ * @x: variable to the function
+ * Return: a pointer to the function
+ */
 int (*get_functions(char x))(va_list)
 {
-    int i = 0;
+	int i = 0;
 
-    spec array[] =
-    {
-        {"c", print_char},
-        {"s", print_string},
-        {"%", print_percentage},
-	{"d", print_d},
-	{"i", print_d},
-        {"NULL", NULL},
-    };
-    while(array[i].p)
-    {
-        if (x == array[i].p[0])
-        	return (array[i].f);
-        i++;
-    }
-    return (NULL);
+	spec array[] =	{
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percentage},
+		{"d", print_d},
+		{"i", print_d},
+		{"NULL", NULL},
+	};
+	while (array[i].p)
+	{
+		if (x == array[i].p[0])
+			return (array[i].f);
+		i++;
+	}
+	return (NULL);
 }
